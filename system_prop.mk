19d8ebadcd1d97104a8552281295f4d5249f010d
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-04-14 02:14:30 -0400

    system_prop: Use system.prop format for properties with spaces

--- system_prop.mk
+++ system_prop.mk
@@ -118,7 +118,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ril.subscription.types=NV,RUIM \
 	DEVICE_PROVISIONED=1 \
 	rild.libpath=/system/lib/libsec-ril.so \
-	rild.libargs=-d/dev/smd0 \
 	ro.multisim.set_audio_params=true \
 	ro.telephony.ril_class=SamsungQcomRIL
 
