94ab1d81978c6e82686c7cca24cb345df0ea2046
GeneticEnginer <igor_cestari@hotmail.com>
2017-05-15 00:09:39 -0300

    Correct last target load freq

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -606,7 +606,7 @@ case "$target" in
                 echo 30000 > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
                 echo 1209600 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
                 echo 0 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
-                echo "1 200000:40 400000:50 533333:60 800000:75 998400:80 1094400:85 1209600:90 1363200:93 1401600:95 1536600:99" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+                echo "1 200000:40 400000:50 533333:60 800000:75 998400:80 1094400:85 1209600:90 1363200:93 1401600:95 1497600:99" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
                 echo 50000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
                 echo 50000 > /sys/devices/system/cpu/cpufreq/interactive/max_freq_hysteresis
 
